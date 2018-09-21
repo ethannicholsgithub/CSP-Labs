426dd27e50bdaf24a2bd80768f8f9060926f567c
ethannicholsgithub <795318@seq.org>
2018-09-21 08:50:17 -0700

    changes

--- Labs/Lab 919 Boids/ball.js
+++ Labs/Lab 919 Boids/ball.js
@@ -5,7 +5,7 @@
 **   Ethan Nichols
 **   Aug 21, 2018
 */
-function Ball(location, velocity, radius, col){
+function Ball(loc, vel, radius, col){
 
   // Instance variables
   this.loc = location;
@@ -24,23 +24,14 @@ function Ball(location, velocity, radius, col){
   // This function changes the location of the ball
   // by adding speed to x and
   this.update = function() {
-    if(this != b1) {
-      var d = this.loc.dist(b1.loc)
-      if (d > 100) {
-        var steeringforce = p5.Vector.sub(b1.loc, this.loc);
-        steeringforce.normalize();
-        steeringforce.mult(0.05);
-        this.vel.add(steeringforce);
-      }
-      if (d < 50) {
-        var steeringforce = p5.Vector.sub(this.loc, b1.loc);
-        steeringforce.normalize();
-        steeringforce.mult(0.05);
-        this.vel.add(steeringforce);
-      }
+    var steeringForce = p5.Vector.sub(b1.loc, this.loc);
+    if (this !== b1) {
+      steeringForce.normalize();
+      steeringForce.mult(0.05);
+      this.vel.add(steeringForce);
+    }
       this.loc.add(this.vel);
     }
-  }
 
   //checkEdges() reverses speed when the ball touches an edge
   this.checkEdges = function(){
--- Labs/Lab 919 Boids/sketch.js
+++ Labs/Lab 919 Boids/sketch.js
@@ -1,6 +1,6 @@
 //  Global variables
-var balls = [];
 var b1;
+var balls = [];
 
 // put setup code here
 function setup() {
@@ -8,14 +8,13 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20,20,20);
+  b1 = new Ball(createVector(width / 2, height / 2), createVector(0, 0), 25, color(255, 0, 0));
   loadBalls(50);
-  b1 = new Ball(createVector(width / 2, height / 2), createVector(0 , 0), 25, color(255, 0, 0));
 
 }
 
 function draw() {
-
-  background(20,20,20);
+  background(20, 20, 20, 1);
   b1.run();
   for(var i = 0; i < balls.length; i = i + 1){
       balls[i].run();
@@ -28,7 +27,7 @@ function loadBalls(numBalls){
 
         var loc = createVector(random(width), random(height));
         var vel = createVector(random(-3,3), random(-3,3));
-        var radius = random(20,40);
+        var radius = 20;
         var col = color(random(255), random(255), random(255));
 
         balls.push(new Ball(loc, vel, radius, col));
