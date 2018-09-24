7ed0f3f1ea109481cbb526596222d6502b3b2a5f
ethannicholsgithub <795318@seq.org>
2018-09-24 08:47:17 -0700

    changed some stuff for boid project

---
+++ Labs/Lab 919 Boids/boids.js
@@ -0,0 +1,44 @@
+
+
+/*
+**   Boid constructer function
+**   Ethan Nichols
+**   Aug 21, 2018
+*/
+function Boid(loc, vel, radius, col){
+
+  // Instance variables
+  this.loc = loc;
+  this.vel = vel;
+  this.rad = radius;
+  this.col = col;
+  this.acc = createVector(0, .1);
+
+  // This function calls other functions
+  this.run = function(){
+    this.checkEdges();
+    this.update();
+    this.render();
+  }
+
+  // This function changes the location of the Boid
+  // by adding speed to x and
+  this.update = function() {
+
+    }
+
+  //checkEdges() reverses speed when the Boid touches an edge
+  this.checkEdges = function(){
+    if(this.loc.x < 0) this.vel.x = -this.vel.x;
+    if(this.loc.x > width) this.vel.x = -this.vel.x;
+    if(this.loc.y < 0) this.vel.y = -this.vel.y;
+    if(this.loc.y > height) this.vel.y = -this.vel.y;
+  }
+
+  // render() draws the Boid at the new location
+  this.render = function(){
+    fill(this.col);
+    triangle(-5, 0, 5, 0, 0, -15);
+  }
+
+}
--- Labs/Lab 919 Boids/sketch.js
+++ Labs/Lab 919 Boids/sketch.js
@@ -1,6 +1,5 @@
 //  Global variables
-var b1;
-var balls = [];
+var boids = [];
 
 // put setup code here
 function setup() {
@@ -8,29 +7,27 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20,20,20);
-  b1 = new Ball(createVector(width / 2, height / 2), createVector(0, 0), 25, color(255, 0, 0));
-  loadBalls(50);
+  loadBoids(50);
 
 }
 
 function draw() {
   background(20, 20, 20, 1);
-  b1.run();
-  for(var i = 0; i < balls.length; i = i + 1){
-      balls[i].run();
+  for(var i = 0; i < boids.length; i = i + 1){
+      boids[i].run();
   }
 }
 
-function loadBalls(numBalls){
+function loadBoids(numBoids){
 
-    for(var i = 0; i < numBalls; i++){
+    for(var i = 0; i < numBoids; i++){
 
         var loc = createVector(random(width), random(height));
         var vel = createVector(random(-3,3), random(-3,3));
         var radius = 20;
         var col = color(random(255), random(255), random(255));
 
-        balls.push(new Ball(loc, vel, radius, col));
+        boids.push(new Boid(loc, vel, radius, col));
 
     }
 
