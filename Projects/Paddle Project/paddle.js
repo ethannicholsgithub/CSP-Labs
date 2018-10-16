94d1df5830602fd9cef43495a8e078c7b1a6d5c2
ethannicholsgithub <795318@seq.org>
2018-10-16 08:49:55 -0700

    gfDGSDVSghghghghggh

--- Projects/Paddle Project/paddle.js
+++ Projects/Paddle Project/paddle.js
@@ -1,28 +1,23 @@
 
 // Paddle function
 function Paddle(loc, clr2){
-
   // letiables for the function
   this.loc = loc; //location
-  //this.vel = vel; //velocity
   this.clr2 = clr2; //color (specifically for the paddle)
-  //this.acc = createVector(0, 0.1);
-  this.wid = 30;
-  this.ht =   10;
+  this.wid = 50;
+  this.ht =  10;
 
   // When the run function is called all of the other functions are called and runs
   this.run = function() {
     this.render();
     this.update();
-
   }
 
   // The update function updates the location / velocity of both the paddle and the objects
   this.update = function() {
     // This code has the paddle move along with the mouse
-    this.loc.x = lerp(this.loc.x, mouseX, 0.05);
+    this.loc.x = lerp(this.loc.x, mouseX, 0.5);
   }
-
 
   // Gives the object a shape and color
   this.render = function() {
--- Projects/Paddle Project/sketch.js
+++ Projects/Paddle Project/sketch.js
@@ -1,70 +1,63 @@
-var balls = [];
-var paddle;
+  var balls = [];
+  var paddle;
 
-function setup() {
-  // hides mouse
-  noCursor();
-  //created the canvas
-  var cnv = createCanvas(800, 800);
-  cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
+  function setup() {
+    // hides mouse
+    noCursor();
+    //created the canvas
+    var cnv = createCanvas(800, 800);
+    cnv.position((windowWidth-width)/2, 30);
+    background(5, 5, 5);
+    fill(200, 30, 150);
 
-  //these two lines define how many balls to load
-  let n = 50;
-  loadBalls(n);
-  loadPaddle(1);
-}
+    //these two lines define how many balls to load
+    let n = 50;
+    loadBalls(n);
+    loadPaddle(1);
+  }
 
-function draw() {
-  //redraws the background with a certain opacity to give that trail as the ball is moving
-  background(5, 5, 5)
-
-  // calls the paddle array
-
+  function draw() {
+    //redraws the background with a certain opacity to give that trail as the ball is moving
+    background(5, 5, 5)
+    // calls the paddle
     paddle.run();
 
-  //paddle[0].run();
-
-  // calls the balls array
-  for (let i = 0; i < balls.length; i++) {
-    balls[i].run();
-  }
-  checkCollision();
-}
-
-  function checkCollision() {
-    for(let i = 0; i < balls.length; i++) {
-      if(balls[i].loc.x > paddle.loc.x &&
-        balls[i].loc.x < (paddle.loc.x + paddle.wid) &&
-        balls[i].loc.y > (paddle.loc.y) &&
-        balls[i].loc.y < (paddle.loc.y + paddle.ht)) {
-
-        balls.splice(i, 1);
-      }
+    // calls the balls array
+    for (let i = 0; i < balls.length; i++) {
+      balls[i].run();
     }
-}
-
-//defines loadBalls
-function loadBalls(numBalls) {
-  for (let i = 0; i < numBalls; i++) {
-    loc = createVector(random(width), random(300, 0));
-    vel = createVector(random(-3, 3), random(-3, 3));
-    clr = color(random(0, 255), random(0, 255), random(0, 255));
-    let b = new Ball(loc, vel, clr);
-    balls.push(b);
+    checkCollision();
   }
 
-// defines loadPaddle
-}
+    function checkCollision() {
+      for(let i = 0; i < balls.length; i++) {
+        if(balls[i].loc.x > paddle.loc.x &&
+          balls[i].loc.x < (paddle.loc.x + paddle.wid) &&
+          balls[i].loc.y > (paddle.loc.y) &&
+          balls[i].loc.y < (paddle.loc.y + paddle.ht)) {
 
-  function loadPaddle() {
+              balls.splice(i, 1);
 
-      let loc = createVector(random(width), random(300, 0));
-      let vel = createVector(random(-3, 3), random(-3, 3));
-      let clr2 = color(255, 0, 0);
-      paddle = new Paddle(loc, clr2);
+        }
+      }
+  }
 
+  //defines loadBalls
+  function loadBalls(numBalls) {
+    for (let i = 0; i < numBalls; i++) {
+      loc = createVector(random(width), random(300, 0));
+      vel = createVector(random(-3, 3), random(-3, 3));
+      clr = color(random(0, 255), random(0, 255), random(0, 255));
+      let b = new Ball(loc, vel, clr);
+      balls.push(b);
+    }
 
+  // defines loadPaddle
+  }
 
-}
+    function loadPaddle() {
+        let loc = createVector(random(width), 400);
+        let vel = createVector(random(-3, 3), random(-3, 3));
+        let clr2 = color(255, 0, 0);
+        paddle = new Paddle(loc, clr2);
+  }
