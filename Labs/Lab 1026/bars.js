efe98704ae15e274bcf2c8c7a9dfd1491d82f49d
ethannicholsgithub <795318@seq.org>
2018-10-30 08:49:11 -0700

    replace code with checkerboard code

--- Labs/Lab 1026/bars.js
+++ Labs/Lab 1026/bars.js
@@ -3,9 +3,11 @@ function Bars(loc, r, g, b, col) {
   this.r = random(0, 255);
   this.g = random(0, 255);
   this.b = random(0, 255);
-  this.col = col;
+  this.col = color(this.r, this.g, this.b);
 
   this.bar = function(){
+    fill(this.col);
+    console.log(this.color);
     rect(10, 10, 10, 10);
   }
 
--- Labs/Lab 1026/sketch.js
+++ Labs/Lab 1026/sketch.js
@@ -1,30 +1,26 @@
 var colorBars = [];
-
+var numberOfBars = 100;
+var barSize;
 //  This is a comment
 //  The setup function function is called once when your program begins
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
-  fill(200, 30, 150);
+  barSize = width / numberOfBars
+  background(100);
+
+  loadBars(100);
 
   for(let i = 0; i < colorBars.length; i++){
-    colorBars.run();
+    colorBars[i].run();
   }
-
-  loadBars(10)
-}
-
-//  The draw function is called @ 30 fps
-function draw() {
-
 }
 
 function loadBars() {
-  for(let i = 0; i < 10; i++){
-    loc = createVector(random(width), 100)
-    col = color(Bars.r, Bars.g, Bars.b);
-    let b = new Bars(loc, Bars.r, Bars.g, Bars.b, col);
-    colorBars.push(b);
+  let rowNum = 0;
+  let columNum = 0;
+
+  for(let row = 0; row < numberOfBars; row++){
+    var y = 8 * row
   }
 }
