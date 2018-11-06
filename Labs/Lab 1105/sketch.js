92ef264651bfdd26f31041a4504c3da7bb5165b2
ethannicholsgithub <795318@seq.org>
2018-11-06 08:46:52 -0800

    included bubble sort

--- Labs/Lab 1105/sketch.js
+++ Labs/Lab 1105/sketch.js
@@ -1,12 +1,27 @@
 var data;
-//  When loading is done, call gotData
+
 function setup(){
    loadJSON("population.json", gotData)
+   bubbleSort();
 }
 function gotData(jData){
    data = jData;
 }
 function draw(){
-  if(data) {
+
+}
+
+function bubbleSort() {
+  var males = data.countrydata.males
+  for (var i = 0; i < males; i++) {
+    for (var j = 0; j < (males - i - 1); j++) {
+      var a = data.countrydata[j].males;
+      var b = data.countrydata[j + 1].males;
+      if(a > b) {
+        var temp = data.countrydata[j].males;
+        data.countrydata[j].males = data.countrydata[j + 1].males;
+        data.countrydata[j + 1].males = temp;
+      }
+    }
   }
 }
