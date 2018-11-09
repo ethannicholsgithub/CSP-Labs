255a3591184c91e1c917d6638c454edb21b38413
ethannicholsgithub <795318@seq.org>
2018-11-09 08:41:50 -0800

    ha yeeet (my lungs hurt)

--- Labs/Lab 1105/sketch.js
+++ Labs/Lab 1105/sketch.js
@@ -1,26 +1,29 @@
-var data;
-
-function setup(){
-   loadJSON("population.json", gotData)
-   bubbleSort();
-}
-function gotData(jData){
-   data = jData;
-}
-function draw(){
-
+var data = [];
+function preload(){
+  data = loadJSON("population.json");
 }
 
-function bubbleSort() {
-  var males = data.countrydata.males
-  for (var i = 0; i < males; i++) {
-    for (var j = 0; j < (males - i - 1); j++) {
-      var a = data.countrydata[j].males;
-      var b = data.countrydata[j + 1].males;
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  backgound(20, 20, 20);
+  bubbleSort(data);
+  console.log(data.countrydata)
+}
+
+function draw() {
+}
+
+function bubbleSort(data) {
+  var length = data.length;
+  for (var i = 0; i < length; i++) {
+    for (var j = 0; j < (length - i - 1); j++);{
+      var a = data.countrydata[j].females;
+      var b = data.countrydata[j + 1].females;
       if(a > b) {
-        var temp = data.countrydata[j].males;
-        data.countrydata[j].males = data.countrydata[j + 1].males;
-        data.countrydata[j + 1].males = temp;
+        var temp = a;
+        a = b;
+        b = temp;
       }
     }
   }
