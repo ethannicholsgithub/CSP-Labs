ef3ff0b29748311068cdd717ad5202c05daf981f
ethannicholsgithub <795318@seq.org>
2018-11-27 08:50:35 -0800

    asdf

--- Projects/Snake Game/food.js
+++ Projects/Snake Game/food.js
@@ -1,28 +1,20 @@
-function Snake(loc, vel, col){
+function Foods(loc, vel){
 
   this.loc = loc;
   this.vel = vel;
-  this.col = col;
 
   this.run = function(){
     this.update();
-    this.render();
+    this.render2();
   }
 
   this.update = function(){
-    this.loc.add(this.vel);
     this.loc.x = constrain(this.loc.x, 0, width-w)
-    if(this.loc.x = constrain(this.loc.x, 0, width-w)){
-    }
     this.loc.y = constrain(this.loc.y, 0, width-w)
-    if(this.loc.y = constrain(this.loc.y, 0, width-w)){
-
-    }
   }
 
-  this.render = function(){
+  this.render2 = function(){
     rect(this.loc.x, this.loc.y, w, w);
-    fill(this.col);
   }
 
 }
--- Projects/Snake Game/sketch.js
+++ Projects/Snake Game/sketch.js
@@ -1,27 +1,40 @@
-//sanke array
+//sanke variable
 var snake;
 //snake segments array
-var w = 2;
+var body = [];
+
+var food;
+
+var w = 20;
+var speed = 20;
 
 function setup() {
     frameRate(10);
     var cnv = createCanvas(800, 800);
     cnv.position(windowWidth-width)/(2, 30);
     background(20, 20, 20);
+
     cols = width/w;
     rows = height/w;
-    var col = color(255, 255, 255);
-    var loc = createVector(width/2, height/2);
-    var vel = createVector(1, 0);
-    snake = new Snake(loc, vel);
+    var loc = createVector(random(cols) * w, random(rows) * w);
+    food = new Foods(loc);
+
+    cols = width/w;
+    rows = height/w;
+    var col = color(255, 0, 0)
+    var loc = createVector(cols *20, rows * 20);
+    var vel = createVector(w, 0);
+    snake = new Snake(loc, vel, col);
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
   background(0, 0, 0);
   snake.run();
+  food.run();
   //call the control function
   keyPressed();
+  segments();
 }
 
 //create segments function to add segments to the Snake
@@ -31,30 +44,31 @@ function segments(){
 }
 //create event function where once snake touches food for   F   U   E   L  that returns a true or false
 function eatFood(){
-
+  if(snake.loc.x === food.loc.x && sanke.loc.y === food.loc.y){
+    null;
+  }
 }
-
 
 //create control function so that the player can control the Snake
 function keyPressed(){
   //if function for if UPARROW is pressed
   if(keyCode === UP_ARROW){
-    snake.loc.add(snake.vel);
-    snake.vel = createVector(0, -w);
+
+    snake.vel = createVector(0, -speed);
   }
   //if function for if DOWNARROW is pressed
   if(keyCode === DOWN_ARROW){
-    snake.loc.add(snake.vel);
-    snake.vel = createVector(0, w);
+
+    snake.vel = createVector(0, speed);
   }
   //if function for if RIGHTARROW is pressed
   if(keyCode === RIGHT_ARROW){
-    snake.loc.add(snake.vel);
-    snake.vel = createVector(w, 0);
+
+    snake.vel = createVector(speed, 0);
   }
   //if function for if LEFTARRROW is pressed POGGGGG
   if(keyCode === LEFT_ARROW){
-    snake.loc.add(snake.vel);
-    snake.vel = createVector(-w, 0);
+
+    snake.vel = createVector(-speed, 0);
   }
 }
